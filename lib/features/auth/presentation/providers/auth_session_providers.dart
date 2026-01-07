3876af79964b08dffee615c58513118d57219fef
DOHYEONWOO\edoro <do-hyeonwoo@yadokari.tv>
2026-01-08 00:22:13 +0900

    refactor: auth関連Providerファイル改善

--- lib/features/auth/presentation/providers/auth_session_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:linky_project_0318/features/auth/presentation/controllers/auth_session_controller.dart';
-
-/// セッション操作（ログアウト等）用。
-final authSessionControllerProvider =
-    AsyncNotifierProvider<AuthSessionController, void>(AuthSessionController.new);
-
-
