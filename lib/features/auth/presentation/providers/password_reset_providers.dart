cb6850ad8403518980340efa3135a69541e6054c
DOHYEONWOO\edoro <do-hyeonwoo@yadokari.tv>
2026-01-08 00:17:06 +0900

    refactor: パスワードリセットProvider統一

--- lib/features/auth/presentation/providers/password_reset_providers.dart
+++ lib/features/auth/presentation/providers/password_reset_providers.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:linky_project_0318/features/auth/di/auth_di.dart';
 import 'package:linky_project_0318/features/auth/presentation/controllers/password_reset_code_controller.dart';
 import 'package:linky_project_0318/features/auth/presentation/controllers/password_reset_controller.dart';
+import 'package:linky_project_0318/features/auth/presentation/controllers/password_reset_new_password_controller.dart';
 import 'package:linky_project_0318/features/auth/presentation/states/password_reset_code_state.dart';
 import 'package:linky_project_0318/features/auth/presentation/states/password_reset_state.dart';
+import 'package:linky_project_0318/features/auth/presentation/states/password_reset_new_password_state.dart';
 
 /// パスワード再設定画面用 StateNotifierProvider。
 final passwordResetControllerProvider =
@@ -15,6 +18,14 @@ final passwordResetControllerProvider =
 final passwordResetCodeControllerProvider = StateNotifierProvider.autoDispose<
     PasswordResetCodeController, PasswordResetCodeState>((ref) {
   return PasswordResetCodeController(ref);
+});
+
+/// 新しいパスワード設定画面用 StateNotifierProvider。
+final passwordResetNewPasswordControllerProvider =
+    StateNotifierProvider.autoDispose<PasswordResetNewPasswordController,
+        PasswordResetNewPasswordState>((ref) {
+  final useCase = ref.watch(resetPasswordUseCaseProvider);
+  return PasswordResetNewPasswordController(ref, useCase);
 });
 
 
